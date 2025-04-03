c77f4fa76ad504eeead7891b85855e52fa5d01bf
SouminiK97 <souminikandula@gmail.com>
2025-04-02 23:34:14 -0700

    deleting bad stuff in gemini

--- model/gemini.py
+++ model/gemini.py
@@ -2,10 +2,6 @@ from flask import Flask, jsonify
 from api import fetch_trivia_questions  # Import the function
 from flask_cors import CORS
 
-
-app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:4887"}})
-
 @app.route('/get-questions', methods=['GET'])
 def get_questions():
     questions = fetch_trivia_questions()
